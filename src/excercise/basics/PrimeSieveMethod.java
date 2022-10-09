a9c1882e97715b58a5bb6d364ee20af624a372a0
shamnad-sherief <sshamnad03@gmail.com>
2022-10-09 13:17:47 +0530

    Prime #SieveOfEratosthenes

---
+++ src/excercise/basics/PrimeSieveMethod.java
@@ -0,0 +1,36 @@
+package excercise.basics;
+
+import java.util.Scanner;
+
+public class PrimeSieveMethod {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int n= input.nextInt();
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+
+        for (int p = 2; p * p <= n; p++) {
+            // If prime[p] is not changed, then it is a
+            // prime
+            if (prime[p] == true) {
+                // Update all multiples of p greater than or
+                // equal to the square of it numbers which
+                // are multiple of p and are less than p^2
+                // are already been marked.
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+
+        // Print all prime numbers
+        for (int i = 2; i <= n; i++) {
+            if (prime[i] == true)
+                System.out.print(i + " ");
+        }
+
+    }
+}
